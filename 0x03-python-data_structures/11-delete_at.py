a8d8e6d6a2d725a81dedd1913bf7f2b291e994cf
michaelgobz <raniahgobz@outlook.com>
2022-06-04 22:38:43 +0000

    added task 11

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    last_index = len(my_list) - 1
+
+    if idx < 0 or idx > last_index:
+        pass
+    else:
+        del my_list[idx]
+    return my_list
+
