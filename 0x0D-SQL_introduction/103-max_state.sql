ab0cfb8ee3c588566be65fdaccd6532a93e321e3
Michael Goboola <raniahgobz@outlook.com>
2022-08-09 13:46:56 -0700

    max value

---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,5 @@
+3 lines (3 sloc)  218 Bytes
+-- displays the max temperature of each state (ordered by State name)
+-- displays the max value of a column in a table
+SELECT `state`, MAX(`value`) 'max_temp' FROM `temperatures` GROUP BY `state` ORDER BY `state` ASC;
+
