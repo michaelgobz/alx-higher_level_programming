7f580cff1b1973f94f73730fdd5adc4ba9b03e89
Michael Goboola <47690301+michaelgobz@users.noreply.github.com>
2022-09-10 12:55:54 +0000

    models and relationships are made and finished

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,15 +2,13 @@
 """Start link class to cities table in database """
 import sys
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import String, Integer , Column
+from sqlalchemy import String, Integer , Column, ForeignKey
 
 Base = declarative_base()
 
 class City(Base):
+    """ City class child of Base"""
     __table__ = 'cities'
     id = Column(Integer,primary_key=True,autoincremnet=True,nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('state.id'), nullable=False)
-
-    def __repr__(self) -> str:
-    return super().__repr__()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,9 +7,7 @@ from sqlalchemy import String, Integer , Column
 Base = declarative_base()
 
 class State(Base):
+    """ State class child of Base"""
     __table__ = 'states'
     id = Column(Integer,primary_key=True,autoincremnet=True,nullable=False)
     name = Column(String(128), nullable=False)
-
-    def __repr__(self) -> str:
-    return super().__repr__()
