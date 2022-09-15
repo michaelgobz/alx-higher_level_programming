7b521f9f747d5bb2b1f99d65e2ba70400d71b0f2
Michael Goboola <47690301+michaelgobz@users.noreply.github.com>
2022-09-15 15:50:53 +0000

    model states and city

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,13 +2,14 @@
 """Start link class to cities table in database """
 import sys
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import String, Integer , Column, ForeignKey
+from sqlalchemy import String, Integer, Column, ForeignKey
 
 Base = declarative_base()
 
+
 class City(Base):
     """ City class child of Base"""
-    __table__ = 'cities'
-    id = Column(Integer,primary_key=True,autoincremnet=True,nullable=False)
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('state.id'), nullable=False)
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,12 +2,14 @@
 """Start link class to state table in database """
 import sys
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import String, Integer , Column
+from sqlalchemy import String, Integer, Column
 
 Base = declarative_base()
 
+
 class State(Base):
     """ State class child of Base"""
-    __table__ = 'states'
-    id = Column(Integer,primary_key=True,autoincremnet=True,nullable=False)
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
