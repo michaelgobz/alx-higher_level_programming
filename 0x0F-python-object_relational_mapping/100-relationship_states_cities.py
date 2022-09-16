d8d55cad7567a583017e4be06bb0fa31248f9246
Michael Goboola <47690301+michaelgobz@users.noreply.github.com>
2022-09-16 12:08:13 +0000

    updated relationship with states and cities

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-""" This script creates the State “California” with the City
+""" This script creates the State â€œCaliforniaâ€ with the City
 San Francisco from the database hbtn_0e_100_usa """
 
 if __name__ == '__main__':
