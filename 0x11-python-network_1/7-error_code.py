b7ff05997d42c801ddb2d8f38fe66ccb92bb589f
Michael Goboola <47690301+michaelgobz@users.noreply.github.com>
2022-09-23 18:03:15 +0000

    capture all error codes and server errors

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+utable File  13 lines (12 sloc)  374 Bytes
+
+#!/usr/bin/python3
+"""This script takes in a URL, sends a request to the URL
+and displays the body of the response"""
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    request = requests.get(url)
+    if request.status_code >= 400:
+        print("Error code: {}".format(request.status_code))
+    else:
+        print(request.text)
