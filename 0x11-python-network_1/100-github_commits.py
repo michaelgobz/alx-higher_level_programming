80d8464b3c88ccba0ea0982a4b53df91cd407abd
Michael Goboola <47690301+michaelgobz@users.noreply.github.com>
2022-09-27 12:30:05 +0000

    taskupdated

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-""" Please list 10 commits (from the most recent to oldest)
-of the repository “rails” by the user “rails”
-You must use the GitHub API, here is the documentation
-https://developer.github.com/v3/repos/commits/
-Print all commits by: `<sha>: <author name>` (one by line)"""
+"""Lists the 10 most recent commits on a given GitHub repository.
+Usage: ./100-github_commits.py <repository name> <repository owner>
+"""
+import sys
+import requests
 
 if __name__ == "__main__":
-    import requests
-    from sys import argv
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-    rep = argv[1]
-    owner = argv[2]
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, rep)
-    request = requests.get(url)
-    count = 0
-    for i in request.json():
-        print(i.get('sha') + ": " + i.get('commit').get('author').get('name'))
-        count += 1
-        if count == 10:
-            break
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
